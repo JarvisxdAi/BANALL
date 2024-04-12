1a88183685697ae8e1e5ea1e68b20ca79bfb1570
JarvisxdAi <123392833+JarvisxdAi@users.noreply.github.com>
2024-04-12 14:27:38 +0530

    Update ban.py

--- ban.py
+++ ban.py
@@ -1,55 +1,20 @@
-#  Copyright (c) 2022 @TheRiZoeL - RiZoeL
-# Telegram Ban All Bot 
-# Creator - RiZoeL
-
 import logging
 import re
 import os
 import sys
 import asyncio
 from telethon import TelegramClient, events
-import telethon.utils
-from telethon.tl import functions
-from telethon.tl.functions.channels import LeaveChannelRequest
-from asyncio import sleep
-from telethon.tl.types import ChatBannedRights, ChannelParticipantsAdmins, ChatAdminRights
+from telethon.tl.types import ChannelParticipantsAdmins, ChannelParticipantsBanned
 from telethon.tl.functions.channels import EditBannedRequest
 from datetime import datetime
 from var import Var
-from time import sleep
 from telethon.errors.rpcerrorlist import FloodWaitError
-from telethon.tl import functions
-from telethon.tl.types import (
-    ChannelParticipantsAdmins,
-    ChannelParticipantsKicked,
-    ChatBannedRights,
-    UserStatusEmpty,
-    UserStatusLastMonth,
-    UserStatusLastWeek,
-    UserStatusOffline,
-    UserStatusOnline,
-    UserStatusRecently,
-)
-
-RIGHTS = ChatBannedRights(
-    until_date=None,
-    view_messages=True,
-    send_messages=True,
-    send_media=True,
-    send_stickers=True,
-    send_gifs=True,
-    send_games=True,
-    send_inline=True,
-    embed_links=True,
-)
-
 
 logging.basicConfig(level=logging.INFO)
 
 print("Starting.....")
 
 Riz = TelegramClient('Riz', Var.API_ID, Var.API_HASH).start(bot_token=Var.BOT_TOKEN)
-
 
 SUDO_USERS = []
 for x in Var.SUDO: 
@@ -71,7 +36,6 @@ async def start(e):
       )
 )
 
-
 @Riz.on(events.NewMessage(pattern="^/ping"))  
 async def ping(e):
     if e.sender_id in SUDO_USERS:
@@ -82,96 +46,62 @@ async def ping(e):
         ms = (end-start).microseconds / 1000
         await event.edit(f"**I'm On** \n\n __Pong__ !! `{ms}` ms")
 
-
-@Riz.on(events.NewMessage(pattern="^/kickall"))
-async def kickall(event):
+@Riz.on(events.NewMessage(pattern="^/cbanall"))
+async def cbanall(event):
    if event.sender_id in SUDO_USERS:
-     if not event.is_group:
-         Reply = f"Noob !! Use This Cmd in Group."
+     if not event.is_channel:
+         Reply = f"Noob !! Use This Cmd in Channel."
          await event.reply(Reply)
      else:
          await event.delete()
          RiZ = await event.get_chat()
-         RiZoeLop = await event.client.get_me()
          admin = RiZ.admin_rights
          creator = RiZ.creator
          if not admin and not creator:
               return await event.reply("I Don't have sufficient Rights !!")
-         RiZoeL = await Riz.send_message(event.chat_id, "**Hello Guyyss*")
-         admins = await event.client.get_participants(event.chat_id, filter=ChannelParticipantsAdmins)
-         admins_id = [i.id for i in admins]
+         RiZoeL = await Riz.send_message(event.chat_id, "**Banning all users...**")
          all = 0
-         kimk = 0
+         banned = 0
          async for user in event.client.iter_participants(event.chat_id):
              all += 1
              try:
-                if user.id not in admins_id:
-                    await event.client.kick_participant(event.chat_id, user.id)
-                    kimk += 1
-                    await asyncio.sleep(0.1)
+                await event.client(EditBannedRequest(event.chat_id, user.id, ChatBannedRights(until_date=None)))
+                banned += 1
+                await asyncio.sleep(0.1)
              except Exception as e:
-                    print(str(e))
-                    await asyncio.sleep(0.1)
-         await RiZoeL.edit(f"**Users Kicked Successfully ! \n\n Kicked:** `{kimk}` \n **Total:** `{all}`")
-    
+                print(str(e))
+                await asyncio.sleep(0.1)
+         await RiZoeL.edit(f"**Users Banned Successfully ! \n\n Banned Users:** `{banned}` \n **Total Users:** `{all}`")
 
-@Riz.on(events.NewMessage(pattern="^/fuckall"))
-async def banall(event):
-   if event.sender_id in SUDO_USERS:
-     if not event.is_group:
-         Reply = f"Randi k grp me cmd use kr !! jb use krna nhi aata toh gaand mraane q aate."
-         await event.reply(Reply)
-     else:
-         await event.delete()
-         RiZ = await event.get_chat()
-         RiZoeLop = await event.client.get_me()
-         admin = RiZ.admin_rights
-         creator = RiZ.creator
-         if not admin and not creator:
-              return await event.reply("I Don't have sufficient Rights !!")
-         RiZoeL = await Riz.send_message(event.chat_id, "**Hello guyss..**")
-         admins = await event.client.get_participants(event.chat_id, filter=ChannelParticipantsAdmins)
-         admins_id = [i.id for i in admins]
-         all = 0
-         bann = 0
-         async for user in event.client.iter_participants(event.chat_id):
-             all += 1
-             try:
-               if user.id not in admins_id:
-                    await event.client(EditBannedRequest(event.chat_id, user.id, RIGHTS))
-                    bann += 1
-                    await asyncio.sleep(0.1)
-             except Exception as e:
-                   print(str(e))
-                   await asyncio.sleep(0.1)
-         await RiZoeL.edit(f"**Users Banned Successfully ! \n\n Banned Users:** `{bann}` \n **Total Users:** `{all}`")
-
-    
 @Riz.on(events.NewMessage(pattern="^/unbanall"))
-async def unban(event):
+async def unbanall(event):
    if event.sender_id in SUDO_USERS:
-     if not event.is_group:
-         Reply = f"Noob !! Use This Cmd in Group."
+     if not event.is_channel:
+         Reply = f"Noob !! Use This Cmd in Channel."
          await event.reply(Reply)
      else:
-         msg = await event.reply("Searching Participant Lists.")
-         p = 0
-         async for i in event.client.iter_participants(event.chat_id, filter=ChannelParticipantsKicked, aggressive=True):
-              rights = ChatBannedRights(until_date=0, view_messages=False)
-              try:
-                await event.client(functions.channels.EditBannedRequest(event.chat_id, i, rights))
-              except FloodWaitError as ex:
-                 print(f"sleeping for {ex.seconds} seconds")
-                 sleep(ex.seconds)
-              except Exception as ex:
-                 await msg.edit(str(ex))
-              else:
-                  p += 1
-         await msg.edit("{}: {} unbanned".format(event.chat_id, p))
-
+         await event.delete()
+         RiZ = await event.get_chat()
+         admin = RiZ.admin_rights
+         creator = RiZ.creator
+         if not admin and not creator:
+              return await event.reply("I Don't have sufficient Rights !!")
+         RiZoeL = await Riz.send_message(event.chat_id, "**Unbanning all users...**")
+         all = 0
+         unbanned = 0
+         async for user in event.client.iter_participants(event.chat_id, filter=ChannelParticipantsBanned):
+             all += 1
+             try:
+                await event.client(EditBannedRequest(event.chat_id, user.id, ChatBannedRights(until_date=0)))
+                unbanned += 1
+                await asyncio.sleep(0.1)
+             except Exception as e:
+                print(str(e))
+                await asyncio.sleep(0.1)
+         await RiZoeL.edit(f"**Users Unbanned Successfully ! \n\n Unbanned Users:** `{unbanned}` \n **Total Users:** `{all}`")
 
 @Riz.on(events.NewMessage(pattern="^/leave"))
-async def _(e):
+async def leave(e):
     if e.sender_id in SUDO_USERS:
         rizoel = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
         if len(e.text) > 7:
@@ -193,7 +123,6 @@ async def _(e):
                 await event.edit("Succesfully Left")
             except Exception as e:
                 await event.edit(str(e))   
-          
 
 @Riz.on(events.NewMessage(pattern="^/restart"))
 async def restart(e):
@@ -207,8 +136,8 @@ async def restart(e):
         os.execl(sys.executable, sys.executable, *sys.argv)
         quit()
 
-
 print("\n\n")
 print("Your Ban All Bot Deployed Successfully ✅")
 
 Riz.run_until_disconnected()
+                 
